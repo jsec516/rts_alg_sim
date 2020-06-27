27119a19ba2ec89580e584b0b73f04a9d57f6caa
Yasin Rahman <yasinshreshto@gmail.com>
2020-06-27 12:11:51 +0000

    Update src/components/task-input.js

--- src/components/task-input.js
+++ src/components/task-input.js
@@ -2,49 +2,52 @@ import React from 'react';
 import { Button, Form } from 'semantic-ui-react'
 
 class TaskInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            taskId: null,
-            exec: null,
-            deadline: null
-        }
-    }
-
-
-    handleChange = (e, { name, value }) => this.setState({ [name]: value })
-
-    handleSubmit = () => {
-        this.props.handler([this.state.taskId, this.state.exec, this.state.deadline]);
-        // this.setState({
-        //     taskId: null,
-        //     exec: null,
-        //     deadline: null
-        // })
-    }
-
-
-    render() {
-        const {taskId, exec, deadline} = this.state;
-        return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group widths='equal'>
-                <Form.Input type="number" id="period" fluid placeholder='Task ID' name='taskId'
-              value={taskId}
-              onChange={this.handleChange}/>
-                <Form.Input type="number" fluid placeholder='Execution Time' name='exec'
-              value={exec}
-              onChange={this.handleChange}/>
-                <Form.Input type="number" fluid placeholder='Deadline' name='deadline'
-              value={deadline}
-              onChange={this.handleChange}/>
-                </Form.Group>
-            
-                <Button primary>Add to task-list</Button>
-            </Form>
-        )
+constructor(props) {
+    super(props);
+    this.state = {
+        taskId: null,
+        exec: null,
+        deadline: null, 
+        simulationtime: null
     }
 }
-  
+
+
+handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
+handleSubmit = () => {
+    this.props.handler([this.state.taskId, this.state.exec, this.state.deadline]);
+    // this.setState({
+    //     taskId: null,
+    //     exec: null,
+    //     deadline: null
+    // })
+}
+
+
+render() {
+    const {taskId, exec, deadline} = this.state;
+    return (
+        
+        <Form onSubmit={this.handleSubmit}>
+            
+            <Form.Group widths='equal'>
+            <Form.Input type="number" id="period" fluid placeholder='Task ID' name='taskId'
+            value={taskId}
+            onChange={this.handleChange}/>
+            <Form.Input type="number" fluid placeholder='Deadline' name='exec'
+            value={exec}
+            onChange={this.handleChange}/>
+            <Form.Input type="number" fluid placeholder='Execution Time' name='deadline'
+            value={deadline}
+            onChange={this.handleChange}/>
+            </Form.Group>
+        
+            <Button primary>Add to task-list</Button>
+        </Form>
+    )
+}
+}
+
 export default TaskInput
-  
+
