82e7f971169f99a33b7ee8273da3b6b215f0f766
MD Jahidul Islam <md.jahidul.islam@pwc.com>
2020-06-29 22:22:22 +0200

    docs added

---
+++ docs/precache-manifest.24b4b2c37809c1eb7b2867f36b8e984d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff38a2a487961faa61ef89ffea35019",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b18f902e87db16fb431",
+    "url": "/static/css/main.f2a4cede.chunk.css"
+  },
+  {
+    "revision": "89a8766c3ff35725bf7d",
+    "url": "/static/js/2.2c3bc8fc.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.2c3bc8fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b18f902e87db16fb431",
+    "url": "/static/js/main.0575d25d.chunk.js"
+  },
+  {
+    "revision": "294580694f88d21013d0",
+    "url": "/static/js/runtime-main.9c6e5fe1.js"
+  }
+]);
